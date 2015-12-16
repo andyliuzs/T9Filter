0bb52d4c1b822e54f734965e2c68f936994cd4fb
andyliuzs <673988904@qq.com>
2015-12-16 22:22:13 +0800

    init project

---
+++ gen/duang/andyliuzs/t9/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package duang.andyliuzs.t9;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/duang/andyliuzs/t9/DuangT9.java
@@ -0,0 +1,16 @@
+package duang.andyliuzs.t9;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class DuangT9 extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.duang_t9_main);
+	}
+
+}
